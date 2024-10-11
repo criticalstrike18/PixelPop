f1738974ebc6f578089f5770b2ec17a134ad19a6
criticalstrike18 <73422754+criticalstrike18@users.noreply.github.com>
2024-10-12 01:15:16 +0530

    firstCommit

---
+++ app/src/main/java/com/saksham/manimereads/bottomNav/bottomNavItem.kt
@@ -0,0 +1,10 @@
+package com.saksham.manimereads.bottomNav
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class BottomNavItem(
+    val name: String,
+    val route: String,
+    val selectedIcon: ImageVector,
+    val unselectedIcon: ImageVector,
+)
